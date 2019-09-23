172adcae10d271cb2d6b0f2f8933140c5716f1fc
thiagorib <39133035+thiagorib@users.noreply.github.com>
2019-09-22 22:32:34 -0300

    Add files via upload

---
+++ MDMatriz.c
@@ -0,0 +1,85 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int i, j, lA, cA, lB, cB, X;
+    printf("\n Informe a quantidade de linhas da matriz A : ");
+    scanf("%d",&lA);
+    printf("\n Informe a quantidade de colunas da matriz A : ");
+    scanf("%d",&cA);
+    printf("\n Informe a quantidade de linhas da matriz B : ");
+    scanf("%d",&lB);
+    printf("\n Informe a quantidade de colunas da matriz B : ");
+    scanf("%d",&cB);
+    float matrizA[lA][cA],matrizB[lB][cB],matrizC[lA][cB],Aux=0,r=0,r1=0;
+    if(cA==lB){
+        for(i=0; i<lA; i++) {
+            for(j=0; j<cA; j++) {
+                if(r<10){
+                    r++;
+                } else {
+                    r=1; 
+                } 
+                matrizA[i][j] = r ;
+            }
+            printf("\n");
+        }
+        for(i=0; i<lB; i++) {        
+            for(j=0; j<cB; j++) {
+                if(r1<10){
+                    r1++;
+                } else {
+                    r1=1; 
+                }
+                matrizB[i][j] = r1;
+            }
+            printf("\n");
+        }
+        printf("Matriz A \n\n");
+        for(i=0; i<lA; i++) {        
+            for(j=0; j<cA; j++) {
+                printf("%6.f", matrizA[i][j]);
+            }
+            printf("\n\n");
+        }
+        printf("Matriz B \n\n");
+        for(i=0; i<lB; i++) {
+            for(j=0; j<cB; j++) {
+                printf("%6.f", matrizB[i][j]);
+            }
+            printf("\n\n");
+        }
+        for(i=0; i<lA; i++) {
+            for(j=0; j<cB; j++) {
+                matrizC[i][j]=0;
+                for(X=0; X<lB; X++) {
+                    Aux += matrizA[i][X] * matrizB[X][j];
+                }
+                matrizC[i][j]=Aux;
+                Aux=0;
+            }
+        }
+        printf("\n\n");
+        printf("Matriz Gerada da Multiplicação: A*B \n\n");
+        for(i=0; i<lA; i++) {
+            for(j=0; j<cB; j++) {
+                printf("%6.f", matrizC[i][j]);
+            }
+            printf("\n\n");
+        }
+        printf("\n\n");
+    } else {
+        printf("\n\n Nao ha como multiplicar as matrizes dadas \n");
+    }
+    system("pause");
+    return 0;
+}
